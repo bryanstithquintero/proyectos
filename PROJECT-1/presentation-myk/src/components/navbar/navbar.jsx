4705026d0b975e9f88bf37675b36e59b804342f5
Bryan Quintero <bryanquinterog@gmail.com>
2023-06-21 16:16:27 -0500

    add icons for examples

--- PROJECT-1/presentation-myk/src/components/navbar/navbar.jsx
+++ PROJECT-1/presentation-myk/src/components/navbar/navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
+import logo from '../../assets/logo.svg'
 
 const navbar = () => {
     return (
-        <div>
-            navbar
+        <div className='salsamentaria_navbar'>
+            <div className='salsamentaria_navbar-links'>
+                <div className='salsamentaria_navbar-links-logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+            </div>
         </div>
     )
 }
