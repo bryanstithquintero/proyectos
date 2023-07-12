ebfea6b2ee2eb6d8472dc5b7b2f4b0f433f3a4d1
Bryan Quintero <bryanquinterog@gmail.com>
2023-07-12 18:35:05 -0500

    navbar begin

--- PROJECT-1/presentation-myk/src/components/navbar/navbar.jsx
+++ PROJECT-1/presentation-myk/src/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+//import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logom.png'
 
 const navbar = () => {
     return (
@@ -9,6 +9,11 @@ const navbar = () => {
             <div className='salsamentaria_navbar-links'>
                 <div className='salsamentaria_navbar-links-logo'>
                     <img src={logo} alt="logo" />
+                </div>
+                <div className='salsamentaria_navbar-links-containers'>
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#about">Nosotros</a></p>
+                    <p><a href="#contactenos">contactenos</a></p>
                 </div>
             </div>
         </div>
